881af51b90f516b385c88f3c658c2c5f59a92787
William Riancho <wr.wllm@gmail.com>
2013-04-08 18:10:35 +0200

    Request::separate

--- trunk/apisources/Request.cpp
+++ trunk/apisources/Request.cpp
@@ -3,18 +3,8 @@
 #include "../HTTPParser.hpp"
 
 Request::Request(char *buffer, int bufferLength)
-  : _body(buffer, bufferLength)
+  : _buffer(NULL), _bufferLength(0), _body(buffer, bufferLength)
 {
-  /*
-  HTTPParser parser(buffer, bufferLength);
-  _header.setCommand(parser.getCmd());
-  _header.setArg(parser.getArg());
-  _header.setVersion(parser.getVersion());
-  for (std::map<std::string, std::string>::const_iterator it = parser.getMap().begin(); it != parser.getMap().end(); ++it)
-    {
-      _header.setValue(it->first, it->second);
-    }
-  */
 }
 
 Request::~Request()
@@ -32,7 +22,7 @@ char	*Request::getBuffer()
   return (_buffer);
 }
 
-RequestHeader &Request::getHeader()
+RequestHeader & Request::getHeader()
 {
   return (_header);
 }
@@ -44,5 +34,19 @@ int Request::getLength() const
 
 void Request::separate()
 {
+  char * body;
+  int buffer_length;
 
+  body = this->_body.getBody();
+  buffer_length = this->_body.getBodyLength();
+
+  HTTPParser parser(body, buffer_length);
+
+  _header.setCommand(parser.getCmd());
+  _header.setArg(parser.getArg());
+  _header.setVersion(parser.getVersion());
+  for (std::map<std::string, std::string>::const_iterator it = parser.getMap().begin(); it != parser.getMap().end(); ++it)
+  {
+    _header.setValue(it->first, it->second);
+  }
 }
